291c8f94af8cbe8a5fc7d180b8ced7e2c3a45226
alexeyseverin <alexxseverin@gmail.com>
2013-07-02 22:25:08 +0800

    Create leg.py

---
+++ leg.py
@@ -0,0 +1,51 @@
+import cpplevel as l
+import sys
+a=l.Long("a.txt")
+b=l.Long("p.txt")
+v=l.Long("1.txt")
+g=f+f
+k=f-f
+if (b<=f or a<=f):
+  print("Wrong number")
+	sys.exit()
+if(b%g==k):
+	print "No answer"
+	sys.exit()
+if(a%b==k):
+  r=k
+  r.outf("le.txt")
+  sys.exit()
+i=g+f
+bb=b/g
+while i<bb:
+	if(b%i==k):
+		print "No answer"
+		sys.exit()
+	i=i+g
+r=f
+t=0
+c=0
+if(b==f):
+	r=1
+  r.outf("le.txt")
+	ssys.exit()
+if(a<k):
+	a=-a
+	if(b%4==3):
+		r=-r
+while 1:
+	t=0
+	while(a%g==k):
+		t=t+1
+		a=a/g
+	if(t%2!=0):
+		if((b%(g*g*g)==(f+g)) or (b%(g*g*g)==(g*g+f))):
+			r=-r
+	if((a%(g*g)==(f+g)) and (b%(g+g)==(g+f))):
+		r=-r
+	c=a
+	a=b%c
+	b=c
+	if(a==k):
+		break
+r.outf("le.txt")
